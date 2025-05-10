dc641c143dbb239010059fe5e3b2d6114371ce60
abdul <ak8871639@gmail.com>
2025-05-10 21:54:37 +0530

    fixes

--- database/migrations/2025_05_05_145550_create_complains_table.php
+++ database/migrations/2025_05_05_145550_create_complains_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name');
-            $table->integer('number');
+            $table->string('number');
             $table->string('email');
             $table->string('service');
             $table->text('address');
